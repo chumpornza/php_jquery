7b742c94e482e7c18b7d427425ddca12215d8204
chumpornza <chumpornza@gmail.com>
2019-10-13 07:54:10 +0000

    add comment

--- create.js
+++ create.js
@@ -1,12 +1,12 @@
 btnSaveFunction = function() {
     
     firstnameTag = $("#firstname");
-    firstname = firstnameTag.val();
+    firstname = firstnameTag.val(); 
     lastnameTag = $("#lastname");
     lastname = lastnameTag.val(); 
     nicknameTag = $("#nickname");
     nickname = nicknameTag.val(); 
-    url = "/update.php?firstname="+firstname+"&"+"lastname="+lastname+"&"+"nickname="+nickname 
+    url = "/update.php?firstname="+firstname+"&"+"lastname="+lastname+"&"+"nickname="+nickname
     successCreateFunction = function(result) {
         
        console.log(result);
